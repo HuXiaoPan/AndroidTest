7f3763e4635ca131165c5c97e8c62f53a324c4c9
HuXiaoPan <hupan008@qq.com>
2018-12-10 23:23:12 +0800

    完成创建表升级表

--- DataBaseTest/app/src/main/java/com/example/admin/databasetest/MainActivity.java
+++ DataBaseTest/app/src/main/java/com/example/admin/databasetest/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.admin.databasetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    MyDatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        databaseHelper = new MyDatabaseHelper(this,"BookStore.db",null,1);
+        databaseHelper = new MyDatabaseHelper(this,"BookStore.db",null,3);
+        Button createDatabase = findViewById(R.id.create_database);
+        createDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseHelper.getWritableDatabase();
+            }
+        });
     }
 }
--- DataBaseTest/app/src/main/java/com/example/admin/databasetest/MyDatabaseHelper.java
+++ DataBaseTest/app/src/main/java/com/example/admin/databasetest/MyDatabaseHelper.java
@@ -1,7 +1,10 @@
 package com.example.admin.databasetest;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MyDatabaseHelper extends SQLiteOpenHelper {
 
@@ -13,7 +16,41 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             "    name text\n" +
             ");";
 
+    public static final String CREATE_CATEGORY = "CREATE Table category (\n" +
+            "    id integer primary key autoincrement,\n" +
+            "    category_name text,\n" +
+            "    category_code integer\n" +
+            ");";
+
+    public static final String CREATE_TESTTABLE = "CREATE Table testtable (\n" +
+            "    id integer primary key autoincrement,\n" +
+            "    category_name text,\n" +
+            "    category_code integer\n" +
+            ");";
+
     private Context mContext;
 
+    //factory参数允许我们查询数据时返回一个自定义的Cursor（？？？）
+    public MyDatabaseHelper( Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
 
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+        db.execSQL(CREATE_TESTTABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE if EXISTS book");
+        db.execSQL("DROP TABLE if EXISTS category");
+        db.execSQL("DROP TABLE if EXISTS testtable");
+        onCreate(db);
+        Toast.makeText(mContext, "升级数据库完成", Toast.LENGTH_LONG).show();
+    }
 }
